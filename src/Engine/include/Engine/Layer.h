98df2933e85e95a4db04e547bb594f4186a99fff
Sebastian <SebastianMOlesen@Gmail.com>
2026-02-18 17:53:22 +0100

    Added a comment, descriping the reason for the boolean in the OnEvent method

--- src/Engine/include/Engine/Layer.h
+++ src/Engine/include/Engine/Layer.h
@@ -6,8 +6,16 @@ class Layer {
        public:
         virtual ~Layer() = default;
 
+        ///
+        /// Processes the events.
+        /// Returns a boolean based on wether the event was consumed or not.
+        /// true: The event was consumed, and will not continue to the later layers.
+        /// false: The event was not consumed, and will be passed along.
+        ///
         virtual bool OnEvent([[maybe_unused]] const sf::Event& event) { return false; }
+
         virtual void OnUpdate([[maybe_unused]] const float deltaTime) {}
+
         virtual void OnRender() {}
 };
 }  // namespace Engine
